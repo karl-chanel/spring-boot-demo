dd2b43baefe69fad709ee4b0b1517d73c736c15f
jaspersteel <jaspersteelxx@gmail.com>
2022-12-09 01:52:10 +0800

    🍻 add 3 class
    Signed-off-by: jaspersteel <jaspersteelxx@gmail.com>

---
+++ demo-ldap/src/main/java/com/xkcoding/ldap/Hola.java
@@ -0,0 +1,14 @@
+package com.xkcoding.ldap;
+
+import java.util.Arrays;
+
+/**
+ * @author jasper
+ * @email jaspersteelxx@gmail.com
+ * @create 2022-12-09 01:44
+ **/
+public class Hola {
+    public static void main(String[] args) {
+        System.out.println("args = " + Arrays.toString(args));
+    }
+}
