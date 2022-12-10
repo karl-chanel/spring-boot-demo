e2927133e1f158a15aa38ed9a27cd694bd0b984b
jaspersteel <jaspersteelxx@gmail.com>
2022-12-10 21:43:46 +0800

    🍻 add 3 class
    Signed-off-by: jaspersteel <jaspersteelxx@gmail.com>

---
+++ demo-ldap/src/main/java/com/xkcoding/ldap/Main.java
@@ -0,0 +1,12 @@
+package com.xkcoding.ldap;
+
+/**
+ * @author jasper
+ * @email jaspersteelxx@gmail.com
+ * @create 2022-12-09 01:43
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("in main");
+    }
+}
---
+++ demo-ldap/src/main/java/com/xkcoding/ldap/Oop.java
@@ -0,0 +1,12 @@
+package com.xkcoding.ldap;
+
+/**
+ * @author jasper
+ * @email jaspersteelxx@gmail.com
+ * @create 2022-12-09 01:50
+ **/
+public class Oop {
+    public static void main(String[] args) {
+
+    }
+}
